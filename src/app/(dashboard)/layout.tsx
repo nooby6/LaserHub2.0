7008772fc8feada0dbf776f4fc4a62953f9fe3fa
nooby6 <giftaudrine1@gmail.com>
2025-03-21 19:00:53 +0300

    fix: update dashboard layout structure to include left and right sections

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -5,5 +5,10 @@ export default function DashboardLayout ({
     children: React.ReactNode;
 }>) {
     return 
-            <div>dashboard{children}</div>;
+            <div className="h-screen flex">{children}
+            {/* LEFT */}
+            <div className="></div>
+            {/* RIGHT */}
+            <div className="></div>
+            </div>;
 }
