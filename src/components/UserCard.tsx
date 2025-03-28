3f3ae413d1331aeeff9b08205ae42530948465ff
nooby6 <giftaudrine1@gmail.com>
2025-03-28 20:09:37 +0300

    fix: update UserCard component to use consistent background colors

--- src/components/UserCard.tsx
+++ src/components/UserCard.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 const UserCard = ({type} : {type:string}) => {
     return (
-        <div className="rounded-2xl odd:bg-lamaPurple even:bg-lamaYellow p-4 flex-1">
+        <div className="rounded-2xl odd:bg-purple even:bg-yellow p-4 flex-1">
             <div className="">
                 <span>2025/03/28</span>
                 <Image src="/more.png" alt="" width={20} height={20}/>
