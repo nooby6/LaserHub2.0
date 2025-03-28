1dc425884f55863d89b823d9e868fb4b722b4db5
nooby6 <giftaudrine1@gmail.com>
2025-03-28 13:03:25 +0300

    feat: add Menu component to dashboard layout with navigation items

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import Image from "next/image";
+import Menu from "@/components/Menu";
 
 export default function DashboardLayout ({
     children,
@@ -15,6 +16,7 @@ export default function DashboardLayout ({
                     <Image src="/logo.png" alt="logo" width={32} height={32}/>
                     <span className="hidden lg:block">Laser Hub Learning Center</span>
                 </Link>
+                <Menu/>
             </div>
             {/* RIGHT */}
             <div className='w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-blue-200'>r</div>
