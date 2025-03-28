4a4e5060086a33d7c30d1c93ce06c0a6e70c2ec4
nooby6 <giftaudrine1@gmail.com>
2025-03-28 19:58:44 +0300

    feat: enhance UserCard component with additional layout elements and styling adjustments

--- src/components/UserCard.tsx
+++ src/components/UserCard.tsx
@@ -1,7 +1,14 @@
+import Image from "next/image";
+
 const UserCard = ({type} : {type:string}) => {
     return (
-        <div className="rounded-2xl odd:bg-lamaPurple even:bg-lamaYellow p-4">
-            test    
+        <div className="rounded-2xl odd:bg-lamaPurple even:bg-lamaYellow p-4 flex-1">
+            <div className="">
+                <span>2025/03/28</span>
+                <Image src="/more.png" alt="" width={20} height={20}/>
+            </div>
+            <h1>12</h1>
+            <h2>{type}</h2>
         </div>
     )
 }
