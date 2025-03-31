cb30fcc367e048e473d9db83c2c51c7a77792c0f
nooby6 <giftaudrine1@gmail.com>
2025-03-31 13:30:28 +0300

    refactor: simplify UserCard component by removing unused props and updating layout

--- src/components/UserCard.tsx
+++ src/components/UserCard.tsx
@@ -1,22 +1,8 @@
-import Image from "next/image";
-
-interface UserCardProps {
-    type: string;
-    date?: string;
-    count?: number;
-}
-
-const UserCard = ({ type, date = "2025/03/28", count = 12 }: UserCardProps) => {
+const UserCard = ({type}:{type:string}) => {
     return (
-        <div className="rounded-2xl odd:bg-purple even:bg-yellow p-4 flex-1 flex flex-col justify-between gap-4 text-center shadow-lg bg-white">
-            <div className="flex justify-between items-center">
-                <span>{date}</span>
-                <Image src="/more.png" alt="More options" width={20} height={20} />
-            </div>
-            <h1>{count}</h1>
-            <h2>{type}</h2>
-        </div>
-    );
-};
+        <div className="rounded-2xl odd:bg-lamaPurpleLight even:bg-lamaYellowLight flex flex-col gap-2 p-4 w-full md:w-1/4 shadow-lg">
 
+        </div>
+    )
+}
 export default UserCard;
