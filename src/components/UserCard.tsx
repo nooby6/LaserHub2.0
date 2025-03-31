979d87d3f9a0b70f042e0ae40cef4f3286b4a9b1
nooby6 <giftaudrine1@gmail.com>
2025-03-31 13:52:00 +0300

    feat: enhance UserCard layout for better responsiveness and visual appeal

--- src/components/UserCard.tsx
+++ src/components/UserCard.tsx
@@ -1,7 +1,14 @@
+import Image from "next/image";
+
 const UserCard = ({type}:{type:string}) => {
     return (
-        <div className="rounded-2xl odd:bg-lamaPurpleLight even:bg-lamaYellowLight flex flex-col gap-2 p-4 w-full md:w-1/4 shadow-lg">
-
+        <div className="rounded-2xl odd:bg-lamaPurple even:bg-lamaYellow flex-1 p-4 w-full md:w-[130px] shadow-lg">
+            <div className="flex justify-between items-center mb-2">
+                <span className="text-[10px] bg-white px-2 py-1 rounded-full text-green-600">2025/03/31</span>
+                <Image src="/more.png" alt="" width={20} height={20} />
+            </div>
+            <h1 className="text-2xl font-semibold my-4">40</h1>
+            <h2>{type}</h2>
         </div>
     )
 }
