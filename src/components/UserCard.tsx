ea20c5cd4596d56bfef231056bd97a87be2e7a80
nooby6 <giftaudrine1@gmail.com>
2025-03-31 14:16:22 +0300

    feat: add CountChart component and update UserCard for improved type display

--- src/components/UserCard.tsx
+++ src/components/UserCard.tsx
@@ -8,7 +8,7 @@ const UserCard = ({type}:{type:string}) => {
                 <Image src="/more.png" alt="" width={20} height={20} />
             </div>
             <h1 className="text-2xl font-semibold my-4">40</h1>
-            <h2>{type}</h2>
+            <h2 className="capitalize text-sm font-medium">{type}</h2>
         </div>
     )
 }
