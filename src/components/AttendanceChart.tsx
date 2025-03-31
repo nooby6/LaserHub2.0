16b6790d8dc6f157ff8a85b0e12283f460ddc194
nooby6 <giftaudrine1@gmail.com>
2025-03-31 19:09:50 +0300

    feat: implement AttendanceChart component and integrate it into AdminPage

--- src/components/AttendanceChart.tsx
+++ src/components/AttendanceChart.tsx
@@ -1,10 +1,86 @@
 "use client";
+import Image from "next/image";
+import {
+  BarChart,
+  Bar,
+  Rectangle,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from "recharts";
 
+const data = [
+  {
+    name: "Mon",
+    present: 30,
+    absent: 10,
+  },
+  {
+    name: "Tue",
+    present: 40,
+    absent: 0,
+  },
+  {
+    name: "Wed",
+    present: 35,
+    absent: 5,
+  },
+  {
+    name: "Thu",
+    present: 9,
+    absent: 31,
+  },
+  {
+    name: "Fri",
+    present: 38,
+    absent: 2,
+  },
+];
 
 const AttendanceChart = () => {
-    return (
-        <div className="">AttendanceChart</div>
-    )
-}
+  return (
+    <div className="bg-white rounded-lg p-4 h-full shadow-lg flex flex-col justify-between">
+      <div className="flex justify-between items-center mb-4">
+        <h1 className="text-lg font-semibold">Attendance</h1>
+        <Image src="/moreDark.png" alt="" width={20} height={20} />
+      </div>
+      <ResponsiveContainer width="100%" height="90%">
+        <BarChart width={500} height={300} data={data} barSize={20}>
+          <CartesianGrid strokeDasharray="3 3" vertical={false} stroke="#ddd" />
+          <XAxis
+            dataKey="name"
+            axisLine={false}
+            tick={{ fill: "#d1d5db" }}
+            tickLine={false}
+          />
+          <YAxis axisLine={false} tick={{ fill: "#d1d5db" }} tickLine={false} />
+          <Tooltip
+            contentStyle={{ borderRadius: "10px", borderColor: "lightgray" }}
+          />
+          <Legend
+            align="left"
+            verticalAlign="top"
+            wrapperStyle={{ paddingTop: "20px", paddingBottom: "40px" }}
+          />
+          <Bar
+            dataKey="present"
+            fill="#FAE27C"
+            legendType="circle"
+            radius={[10, 10, 0, 0]}
+          />
+          <Bar
+            dataKey="absent"
+            fill="#C3EBFA"
+            legendType="circle"
+            radius={[10, 10, 0, 0]}
+          />
+        </BarChart>
+      </ResponsiveContainer>
+    </div>
+  );
+};
 
 export default AttendanceChart;
