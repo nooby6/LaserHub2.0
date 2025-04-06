061b3745d4b06b595573ac7bf0c35dbd35dce476
nooby6 <giftaudrine1@gmail.com>
2025-04-06 16:48:19 +0300

    feat: update announcement title for clarity in Announcements component

--- src/components/Announcements.tsx
+++ src/components/Announcements.tsx
@@ -8,7 +8,7 @@ const Announcements = () => {
         <div className="flex flex-col gap-4 mt-4">
           <div className="bg-lamaSkyLight rounded-md p-4">
             <div className="flex items-center justify-between">
-              <h2 className="font-medium">Lorem ipsum dolor sit</h2>
+              <h2 className="font-medium">Exam Preparation</h2>
               <span className="text-xs text-gray-400 bg-white rounded-md px-1 py-1">
                 2025-01-01
               </span>
