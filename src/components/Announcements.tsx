b4851f3f0b170b6ced5f406fe1a9b0603fb561f1
nooby6 <giftaudrine1@gmail.com>
2025-04-13 18:06:33 +0300

    fix: add shadow styles to Announcements component for improved visual depth

--- src/components/Announcements.tsx
+++ src/components/Announcements.tsx
@@ -1,12 +1,12 @@
 const Announcements = () => {
     return (
-      <div className="bg-white p-4 rounded-md">
+      <div className="bg-white p-4 rounded-md shadow-md shadow-lg h-full">
         <div className="flex items-center justify-between">
           <h1 className="text-xl font-semibold">Announcements</h1>
           <span className="text-xs text-gray-400">View All</span>
         </div>
         <div className="flex flex-col gap-4 mt-4">
-          <div className="bg-laserSkyLight rounded-md p-4">
+          <div className="bg-laserSkyLight rounded-md p-4 shadow-md shadow-lg">
             <div className="flex items-center justify-between">
               <h2 className="font-medium">Exam Preparation</h2>
               <span className="text-xs text-gray-400 bg-white rounded-md px-1 py-1">
@@ -18,7 +18,7 @@ const Announcements = () => {
               expedita. Rerum, quidem facilis?
             </p>
           </div>
-          <div className="bg-laserPurpleLight rounded-md p-4">
+          <div className="bg-laserPurpleLight rounded-md p-4 shadow-md shadow-lg">
             <div className="flex items-center justify-between">
               <h2 className="font-medium">Lorem ipsum dolor sit</h2>
               <span className="text-xs text-gray-400 bg-white rounded-md px-1 py-1">
@@ -30,7 +30,7 @@ const Announcements = () => {
               expedita. Rerum, quidem facilis?
             </p>
           </div>
-          <div className="bg-laserYellowLight rounded-md p-4">
+          <div className="bg-laserYellowLight rounded-md p-4 shadow-md shadow-lg">
             <div className="flex items-center justify-between">
               <h2 className="font-medium">Lorem ipsum dolor sit</h2>
               <span className="text-xs text-gray-400 bg-white rounded-md px-1 py-1">
