518ae59e4adde6d0cfe18ade79bb15d0a39e27a1
nooby6 <giftaudrine1@gmail.com>
2025-04-13 14:40:39 +0300

    fix: remove overflow-scroll from main content section in DashboardLayout

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -24,7 +24,7 @@ export default function DashboardLayout({
             </div>
             
             {/* RIGHT: Main content section */}
-            <div className='w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#F7F8FA] overflow-scroll'>
+            <div className='w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#F7F8FA]'>
                 {/* Navbar component */}
                 <Navbar />
                 {/* Dynamic content passed as children */}
