8c2ca7b2bfac103e1400ff85f3c16a50352f28a5
nooby6 <giftaudrine1@gmail.com>
2025-04-13 18:00:49 +0300

    fix: add h-full class to StudentPage container for proper height adjustment

--- src/app/(dashboard)/student/page.tsx
+++ src/app/(dashboard)/student/page.tsx
@@ -7,7 +7,7 @@ const StudentPage = () => {
     <div className="p-4 flex gap-4 flex-col xl:flex-row">
       {/* LEFT */}
       <div className="w-full xl:w-2/3">
-        <div className="h-full bg-white p-4 rounded-md">
+        <div className="h-full bg-white p-4 rounded-md shadow-md shadow-lg">
           <h1 className="text-xl font-semibold">Schedule (4A)</h1>
           <BigCalendar/>
         </div>
