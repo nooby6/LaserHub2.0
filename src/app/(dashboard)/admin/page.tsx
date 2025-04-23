22ffd1ddb42765ebc2cf24361499fb46ab67f794
nooby6 <giftaudrine1@gmail.com>
2025-04-23 22:41:18 +0300

    refactor: enhance AdminPage layout with improved spacing and styling

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -10,11 +10,11 @@ import Announcements from "@/components/Announcements";
 
 const AdminPage = () => {
     return (
-        <div className="p-4 flex gap-4 flex-col md:flex-row">
+        <div className="p-4 flex gap-4 flex-col md:flex-row xl:flex-row xl:gap-8">
             {/* LEFT SIDE */}
-            <div className="w-full lg:w-2/3 flex flex-col gap-8">
+            <div className="w-full lg:w-2/3 flex flex-col gap-8 bg-gray p-4 rounded-md">
             {/* USER CARDS */}
-            <div className="flex gap-4 justify-between flex-wrap">
+            <div className="flex gap-4 justify-between flex-wrap md:flex-nowrap">
                 <UserCard type="Students"/>
                 <UserCard type="Teachers"/>
                 <UserCard type="Parents"/>
@@ -24,7 +24,7 @@ const AdminPage = () => {
             <div className="flex gap-4 flex-col lg:flex-row">
             
                 {/* COUNT CHART */}
-                <div className="w-full lg:w-1/3 h-[450px]">
+                <div className="w-full lg:w-1/3 h-[450px] lg:h-[450px]">
                     <CountChart/>
                 </div>
             
@@ -34,7 +34,7 @@ const AdminPage = () => {
                 </div>
             </div>
             {/* BOTTOM CHART */}
-            <div className="w-full h-[500px]">
+            <div className="w-full h-[500px] lg:h-[450px]">
                 <FinanceChart/>
             </div>
             </div>
