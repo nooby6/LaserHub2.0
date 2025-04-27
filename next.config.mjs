19ae83385b32d8087cb5a5fb1d1cdf01c7cf0725
nooby6 <giftaudrine1@gmail.com>
2025-04-27 14:49:11 +0300

    feat: configure image optimization with remote patterns for Pexels

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'images.pexels.com',
+          pathname: '/**',  // Allow all images from Pexels
+        },
+      ],
+    },
+  };
+  
+  export default nextConfig;
+  
