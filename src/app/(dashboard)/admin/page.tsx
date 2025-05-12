c6098d114c76aa2c6186851e28b31dd8f8a7bb1b
nooby6 <giftaudrine1@gmail.com>
2025-05-12 15:33:58 +0300

    feat: refactor AdminPage layout and replace chart components with containers for improved structure and functionality

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,63 +1,49 @@
-import AttendanceChart from "@/components/AttendanceChart";
-import CountChart from "@/components/CountChart";
+import Announcements from "@/components/Announcements";
+import AttendanceChartContainer from "@/components/AttendanceChartContainer";
+import CountChartContainer from "@/components/CountChartContainer";
+import EventCalendarContainer from "@/components/EventCalendarContainer";
 import FinanceChart from "@/components/FinanceChart";
 import UserCard from "@/components/UserCard";
-import EventCalendar from "@/components/EventCalender";
-import 'react-calendar/dist/Calendar.css';
-import Announcements from "@/components/Announcements";
 
-// AdminPage component serves as the main dashboard for the admin interface.
-// It is divided into two main sections: the left side (charts and user cards) and the right side (calendar and announcements).
-const AdminPage = () => {
-    return (
-        <div className="p-4 flex gap-4 flex-col md:flex-row xl:flex-row xl:gap-8">
-            {/* LEFT SIDE */}
-            <div className="w-full lg:w-2/3 flex flex-col gap-8 bg-gray p-4 rounded-md bg-gray">
-                {/* USER CARDS */}
-                {/* Displays user statistics for different user types: Students, Teachers, Parents, and Staff */}
-                <div className="flex gap-4 justify-between flex-wrap md:flex-nowrap">
-                    <UserCard type="Students"/>
-                    <UserCard type="Teachers"/>
-                    <UserCard type="Parents"/>
-                    <UserCard type="Staff"/>
-                </div>
-
-                {/* MIDDLE CHARTS */}
-                {/* Contains two charts: CountChart and AttendanceChart */}
-                <div className="flex gap-4 flex-col lg:flex-row">
-                    {/* COUNT CHART */}
-                    {/* Displays a chart for count-related data */}
-                    <div className="w-full lg:w-1/3 h-[450px] lg:h-[450px]">
-                        <CountChart/>
-                    </div>
-                    
-                    {/* ATTENDANCE CHART */}
-                    {/* Displays a chart for attendance-related data */}
-                    <div className="w-full lg:w-2/3 h[450px]">
-                        <AttendanceChart/>
-                    </div>
-                </div>
-
-                {/* BOTTOM CHART */}
-                {/* Displays a financial chart */}
-                <div className="w-full h-[500px] lg:h-[450px]">
-                    <FinanceChart/>
-                </div>
-            </div>
-
-            {/* RIGHT SIDE */}
-            {/* Contains the Event Calendar and Announcements section */}
-            <div className="w-full lg:w-1/3 flex flex-col gap-8">
-                {/* EVENT CALENDAR */}
-                {/* Displays a calendar for events */}
-                <EventCalendar/>
-                
-                {/* ANNOUNCEMENTS */}
-                {/* Displays announcements for the admin */}
-                <Announcements/>
-            </div>
+const AdminPage = ({
+  searchParams,
+}: {
+  searchParams: { [keys: string]: string | undefined };
+}) => {
+  return (
+    <div className="p-4 flex gap-4 flex-col md:flex-row">
+      {/* LEFT */}
+      <div className="w-full lg:w-2/3 flex flex-col gap-8">
+        {/* USER CARDS */}
+        <div className="flex gap-4 justify-between flex-wrap">
+          <UserCard type="admin" />
+          <UserCard type="teacher" />
+          <UserCard type="student" />
+          <UserCard type="parent" />
         </div>
-    )
-}
+        {/* MIDDLE CHARTS */}
+        <div className="flex gap-4 flex-col lg:flex-row">
+          {/* COUNT CHART */}
+          <div className="w-full lg:w-1/3 h-[450px]">
+            <CountChartContainer />
+          </div>
+          {/* ATTENDANCE CHART */}
+          <div className="w-full lg:w-2/3 h-[450px]">
+            <AttendanceChartContainer />
+          </div>
+        </div>
+        {/* BOTTOM CHART */}
+        <div className="w-full h-[500px]">
+          <FinanceChart />
+        </div>
+      </div>
+      {/* RIGHT */}
+      <div className="w-full lg:w-1/3 flex flex-col gap-8">
+        <EventCalendarContainer searchParams={searchParams}/>
+        <Announcements />
+      </div>
+    </div>
+  );
+};
 
 export default AdminPage;
