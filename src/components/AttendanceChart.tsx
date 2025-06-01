a2d69759c88b3c12e786e0abb05bb2ebe3d8c472
nooby6 <giftaudrine1@gmail.com>
2025-06-01 18:41:53 +0300

    refactor: update AttendanceChart and CountChart to accept dynamic data and fix styling issues in UserCard

--- src/components/AttendanceChart.tsx
+++ src/components/AttendanceChart.tsx
@@ -12,74 +12,44 @@ import {
   ResponsiveContainer,
 } from "recharts";
 
-const data = [
-  {
-    name: "Mon",
-    Present: 30,
-    Absent: 10,
-  },
-  {
-    name: "Tue",
-    Present: 40,
-    Absent: 0,
-  },
-  {
-    name: "Wed",
-    Present: 35,
-    Absent: 5,
-  },
-  {
-    name: "Thu",
-    Present: 9,
-    Absent: 31,
-  },
-  {
-    name: "Fri",
-    Present: 38,
-    Absent: 2,
-  },
-];
-
-const AttendanceChart = () => {
+const AttendanceChart = ({
+  data,
+}: {
+  data: { name: string; present: number; absent: number }[];
+}) => {
   return (
-    <div className="bg-white rounded-lg p-4 h-full shadow-lg flex flex-col justify-between w-full text-black-500">
-      <div className="flex justify-between items-center mb-4">
-        <h1 className="text-lg font-semibold">Attendance</h1>
-        <Image src="/moreDark.png" alt="" width={20} height={20} />
-      </div>
-      <ResponsiveContainer width="100%" height="90%">
-        <BarChart width={500} height={300} data={data} barSize={20}>
-          <CartesianGrid strokeDasharray="3 3" vertical={false} stroke="#ddd" />
-          <XAxis
-            dataKey="name"
-            axisLine={false}
-            tick={{ fill: "black" }}
-            tickLine={false}
-          />
-          <YAxis axisLine={false} tick={{ fill: "black" }} tickLine={false} />
-          <Tooltip
-            contentStyle={{ borderRadius: "10px", borderColor: "lightgray" }}
-          />
-          <Legend
-            align="left"
-            verticalAlign="top"
-            wrapperStyle={{ paddingTop: "20px", paddingBottom: "40px" }}
-          />
-          <Bar
-            dataKey="Present"
-            fill="#FAE27C"
-            legendType="circle"
-            radius={[10, 10, 0, 0]}
-          />
-          <Bar
-            dataKey="Absent"
-            fill="#C3EBFA"
-            legendType="circle"
-            radius={[10, 10, 0, 0]}
-          />
-        </BarChart>
-      </ResponsiveContainer>
-    </div>
+    <ResponsiveContainer width="100%" height="90%">
+      <BarChart width={500} height={300} data={data} barSize={20}>
+        <CartesianGrid strokeDasharray="3 3" vertical={false} stroke="black" />
+        <XAxis
+          dataKey="name"
+          axisLine={false}
+          tick={{ fill: "black" }}
+          tickLine={false}
+        />
+        <YAxis axisLine={false} tick={{ fill: "black" }} tickLine={false} />
+        <Tooltip
+          contentStyle={{ borderRadius: "10px", borderColor: "lightgray" }}
+        />
+        <Legend
+          align="left"
+          verticalAlign="top"
+          wrapperStyle={{ paddingTop: "20px", paddingBottom: "40px" }}
+        />
+        <Bar
+          dataKey="present"
+          fill="#FAE27C"
+          legendType="circle"
+          radius={[10, 10, 0, 0]}
+        />
+        <Bar
+          dataKey="absent"
+          fill="#C3EBFA"
+          legendType="circle"
+          radius={[10, 10, 0, 0]}
+        />
+      </BarChart>
+    </ResponsiveContainer>
   );
 };
 
--- src/components/CountChart.tsx
+++ src/components/CountChart.tsx
@@ -7,67 +7,46 @@ import {
   ResponsiveContainer,
 } from "recharts";
 
-const data = [
-  {
-    name: "Total",
-    count: 40,
-    fill: "white",
-  },
-  {
-    name: "Girls",
-    count: 20,
-    fill: "#FAE27C",
-  },
-  {
-    name: "Boys",
-    count: 20,
-    fill: "#C3EBFA",
-  },
-];
 
-const CountChart = () => {
+const CountChart = ({ boys, girls }: { boys: number; girls: number }) => {
+  const data = [
+    {
+      name: "Total",
+      count: boys+girls,
+      fill: "white",
+    },
+    {
+      name: "Girls",
+      count: girls,
+      fill: "#FAE27C",
+    },
+    {
+      name: "Boys",
+      count: boys,
+      fill: "#C3EBFA",
+    },
+  ];
   return (
-    <div className="bg-white rounded-xl w-full h-full p-4 shadow-lg flex flex-col justify-between">
-      {/* TITLE */}
-      <div className="flex justify-between items-center mb-4">
-        <h1 className="text-lg font-semibold">Students</h1>
-        <Image src="/moreDark.png" alt="" width={20} height={20} />
-      </div>
-      {/* CHART */}
-      <div className="relative w-full h-[75%]">
-        <ResponsiveContainer>
-          <RadialBarChart
-            cx="50%"
-            cy="50%"
-            innerRadius="40%"
-            outerRadius="100%"
-            barSize={32}
-            data={data}
-          >
-            <RadialBar background dataKey="count" />
-          </RadialBarChart>
-        </ResponsiveContainer>
-        <Image
-          src="/maleFemale.png"
-          alt=""
-          width={50}
-          height={50}
-          className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
-        />
-      </div>
-      {/* BOTTOM */}
-      <div className="flex justify-center gap-16">
-        <div className="flex flex-col gap-1">
-          <div className="w-5 h-5 bg-laserSky rounded-full" />
-          <h1 className="font-bold">20</h1>
-          <h2 className="text-xs">Boys (50%)</h2>
-        </div>
-        <div className="flex flex-col gap-1">
-          <div className="w-5 h-5 bg-laserYellow rounded-full" />
-          <h1 className="font-bold">20</h1>
-          <h2 className="text-xs">Girls (50%)</h2>
-        </div>
-      </div>
+    <div className="relative w-full h-[75%]">
+      <ResponsiveContainer>
+        <RadialBarChart
+          cx="50%"
+          cy="50%"
+          innerRadius="40%"
+          outerRadius="100%"
+          barSize={32}
+          data={data}
+        >
+          <RadialBar background dataKey="count" />
+        </RadialBarChart>
+      </ResponsiveContainer>
+      <Image
+        src="/maleFemale.png"
+        alt=""
+        width={50}
+        height={50}
+        className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+      />
     </div>
   );
 };
