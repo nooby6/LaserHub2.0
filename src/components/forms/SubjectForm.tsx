363bd1f26f43e08e41b471d89ed39a741597467e
nooby6 <giftaudrine1@gmail.com>
2025-06-02 19:29:35 +0300

    fix: update ClassForm to correctly set hidden input type and handle optional grades mapping

--- src/components/forms/SubjectForm.tsx
+++ src/components/forms/SubjectForm.tsx
@@ -120,7 +120,7 @@ const ClassForm = ({
             defaultValue={data?.id}
             register={register}
             error={errors?.id}
-            hidden
+            type="hidden"
           />
         )}
         <div className="flex flex-col gap-2 w-full md:w-1/4">
@@ -155,7 +155,7 @@ const ClassForm = ({
             {...register("gradeId")}
             defaultValue={data?.gradeId}
           >
-            {grades.map((grade: { id: number; level: number }) => (
+            {grades?.map((grade: { id: number; level: number }) => (
               <option
                 value={grade.id}
                 key={grade.id}
