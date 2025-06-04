ec9dc6157dcbef0f6cdf1617ea41e82244e229a8
nooby6 <giftaudrine1@gmail.com>
2025-06-04 18:56:46 +0300

    feat: add Cloudinary integration for image handling in configuration and dependencies

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      remotePatterns: [
-        {
-          protocol: 'https',
-          hostname: 'images.pexels.com',
-          pathname: '/**',  // Allow all images from Pexels
-        },
-      ],
-    },
-  };
-  
-  export default nextConfig;
-  
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.pexels.com',
+        pathname: '/**',  // Allow all images from Pexels
+      },
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        pathname: '/**',  // Allow all images from Cloudinary
+      },
+    ],
+  },
+};
+
+export default nextConfig;
