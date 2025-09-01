58b930a1d272b8968010a2fe0a51ba2d9b578eb7
nooby6 <giftaudrine1@gmail.com>
2025-09-01 15:32:20 +0300

    fix: update color classes in FormModal for consistency

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -19,7 +19,7 @@ export default function DashboardLayout({
           <Image src="/logo.png" alt="logo" width={32} height={32} />
           <span className="hidden lg:block font-bold">Laser Hub</span>
         </Link>
-        <Menu role={""} />
+        <Menu />
       </div>
       {/* RIGHT */}
       <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#F7F8FA] overflow-scroll flex flex-col">
